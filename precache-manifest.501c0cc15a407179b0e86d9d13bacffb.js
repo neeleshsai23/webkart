78967be9c2a826d13fff24e77b6a83fc5fbd1258
Neelesh Sai <rneeleshsai@gmail.com>
2020-09-05 15:10:24 +0530

    Updates

---
+++ precache-manifest.501c0cc15a407179b0e86d9d13bacffb.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "677ce70b28a2d76547d4186066115114",
+    "url": "/webkart/index.html"
+  },
+  {
+    "revision": "1fd077988d9dfb75528c",
+    "url": "/webkart/static/css/main.9d994a53.chunk.css"
+  },
+  {
+    "revision": "297d8a44b0691b54a53c",
+    "url": "/webkart/static/js/2.e09791c5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/webkart/static/js/2.e09791c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fd077988d9dfb75528c",
+    "url": "/webkart/static/js/main.1b3a56f2.chunk.js"
+  },
+  {
+    "revision": "7b2bbc59891847d0f586",
+    "url": "/webkart/static/js/runtime-main.9862ce87.js"
+  },
+  {
+    "revision": "ad933bb1874441cf683fb8f5ca37384b",
+    "url": "/webkart/static/media/iphone11.ad933bb1.png"
+  },
+  {
+    "revision": "d1b3fae20c8d028b8c3d72ba70b4242c",
+    "url": "/webkart/static/media/oppoa12.d1b3fae2.jpg"
+  },
+  {
+    "revision": "d9c3307e6c272a2f3fc6995d834c2656",
+    "url": "/webkart/static/media/oppof11.d9c3307e.jpg"
+  },
+  {
+    "revision": "08f39c93615a6369fa3df0f5b5d75410",
+    "url": "/webkart/static/media/realme5pro.08f39c93.jpg"
+  },
+  {
+    "revision": "a84b6d4a0f51c819e966f5dd7cc55ade",
+    "url": "/webkart/static/media/redmi8a.a84b6d4a.jpg"
+  },
+  {
+    "revision": "ccb8f606e86cc42e33c16a824767f749",
+    "url": "/webkart/static/media/redminote9pro.ccb8f606.jpg"
+  },
+  {
+    "revision": "5d97e99d423d64aa71e59df93a158f39",
+    "url": "/webkart/static/media/s10.5d97e99d.jpg"
+  }
+]);
